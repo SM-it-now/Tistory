ae7d4bebdbd47540a88911170a90ab41f2fec49f
SM-it-now <ibb09@naver.com>
2021-11-23 00:01:07 +0900

    21.11.23 django Taggit을 이용한 태그 구현.

--- blogs/models.py
+++ blogs/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from helpers.models import BaseModel
 from users.models import User
 
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
 # 게시글 기능
@@ -12,6 +14,8 @@ class Post(BaseModel):
     image = models.ImageField(blank=True, null=True)
 
     likes = models.ManyToManyField(User, related_name='likes', blank=True)
+
+    tags = TaggableManager()
 
     def __str__(self):
         return '{} : {} / {}'.format(self.id, self.title, self.user)
